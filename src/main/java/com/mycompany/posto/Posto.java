b989b248cbb109185799b708f53e46919e96da75
Raul Muniz <136410938+RaulSMuniz@users.noreply.github.com>
2023-10-06 00:44:54 -0300

    Update Posto.java

--- src/main/java/com/mycompany/posto/Posto.java
+++ src/main/java/com/mycompany/posto/Posto.java
@@ -5,11 +5,14 @@ public class Posto {
 
     public static void main(String[] args) {
         Posto2 p = new Posto2();
+        p.alcool = 5;
+        p.gasolina = 7;
+        
         int opcao = 0;
         opcao = Integer.parseInt(JOptionPane.showInputDialog(""
         + "=-=-=-=- Menu -=-=-=-= \n"
-        + "1. Álcool \n"
-        + "2. Gasolina \n"
+        + "1. Álcool (5 reais por litro) \n"
+        + "2. Gasolina (7 reais por litro) \n"
         + "Escolha uma opção."));
         if (opcao == 1){
             p.litro = Double.parseDouble(JOptionPane.showInputDialog ("Quantos litros de álcool você deseja?"));
