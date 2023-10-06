619357568b874d855aad80f94c0485cd286f76b5
Raul Muniz <136410938+RaulSMuniz@users.noreply.github.com>
2023-10-06 00:48:25 -0300

    Update Posto2.java

--- src/main/java/com/mycompany/posto/Posto2.java
+++ src/main/java/com/mycompany/posto/Posto2.java
@@ -4,31 +4,25 @@ public class Posto2 {
     public double alcool, gasolina, litro, desconto, alresult, garesult;
     public double al(){
             if (litro <= 20){
-            desconto = 0.03;
-            while (litro <= 20){
-                alresult = (alcool * litro) - (litro * desconto);
-                return alresult;
-            }
-        } else {
-            desconto = 0.06;
-            alresult = (alcool * litro) - (litro * desconto);
+                    desconto = litro * 0.03;
+                    alresult = (alcool * litro) - (desconto);
+                    return alresult;               
+            } else {
+            desconto = litro * 0.06;
+            alresult = (alcool * litro) - (desconto);
            return alresult;
-        }
-            return alresult;
+            }
         }
     public double gas(){
             if (litro <= 20){
-            desconto = 0.04;
-            while (litro <= 20){
-                garesult = (gasolina * litro) - (litro * desconto);
+                    desconto = litro * 0.04;
+                    garesult = (gasolina * litro) - (desconto);
+                    return garesult;
+            } else {
+                desconto = litro * 0.05;
+                garesult = (gasolina * litro) - (desconto);
                 return garesult;
-            }
-        } else {
-            desconto = 0.06;
-            garesult = (gasolina * litro) - (litro * desconto);
-            return garesult;
-            }
-            return garesult;
+                }
         }
     }
 
